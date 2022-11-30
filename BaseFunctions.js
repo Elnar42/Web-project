20112747d57859947f46a187cc277d037cb83ccd
Elnar42 <118639055+Elnar42@users.noreply.github.com>
2022-11-30 14:27:14 +0400

    Add files via upload

---
+++ BaseFunctions.js
@@ -0,0 +1,27 @@
+function Ada() {
+    document.getElementById("Ada0").style.display = "block";
+    document.getElementById("CodeEvent").style.display = "none";
+    document.getElementById("Team").style.display = "none";
+    document.getElementById("Project").style.display = "none";
+}
+
+function CodeEvent() {
+    document.getElementById("Ada0").style.display = "none";
+    document.getElementById("CodeEvent").style.display = "block";
+    document.getElementById("Team").style.display = "none";
+    document.getElementById("Project").style.display = "none";
+}
+
+function Team() {
+    document.getElementById("Ada0").style.display = "none";
+    document.getElementById("CodeEvent").style.display = "none";
+    document.getElementById("Team").style.display = "block";
+    document.getElementById("Project").style.display = "none";
+}
+
+function Project() {
+    document.getElementById("Ada0").style.display = "none";
+    document.getElementById("CodeEvent").style.display = "none";
+    document.getElementById("Team").style.display = "none";
+    document.getElementById("Project").style.display = "block";
+}
